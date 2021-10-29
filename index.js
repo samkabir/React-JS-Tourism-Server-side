895906923f1e2e497de506f1419d0e1c6a6535da
samkabir <samkabir26@gmail.com>
2021-10-30 00:01:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qmw5x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('database connected!');
+    }
+    finally{
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
